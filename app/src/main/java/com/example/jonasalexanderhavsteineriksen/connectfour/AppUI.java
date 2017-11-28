d2b902fafc49ed48b8dce2b6d3baa63da2e89f37
Jonas Havstein Eriksen <jonas.havstein@gmail.com>
2017-11-28 14:56:27 +0100

    Generalized to up to four-player connect-4.

--- app/src/main/java/com/example/jonasalexanderhavsteineriksen/connectfour/AppUI.java
+++ app/src/main/java/com/example/jonasalexanderhavsteineriksen/connectfour/AppUI.java
@@ -1,5 +1,6 @@
 package com.example.jonasalexanderhavsteineriksen.connectfour;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatButton;
